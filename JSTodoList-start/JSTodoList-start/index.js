f23dd025e2d919f06002d05c4c1b1864bcba5ddc
Nnatalia Sancen <21031041@itcelaya.edu.mx>
2025-03-25 23:50:14 -0600

    Get values test 3

--- JSTodoList-start/JSTodoList-start/index.js
+++ JSTodoList-start/JSTodoList-start/index.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function (){
     const title = document.getElementById('title');
     const description = document.getElementById('description');
+    const table = document.getElementById('table');
     const btn = document.getElementById('add');
 
     btn.onclick = function (){
         console.log('Title:', title.value);
         console.log('Description:', description.value);
     }
-})
+
+    function addTodo(){
+if (title.value == '')
+    }
+});
